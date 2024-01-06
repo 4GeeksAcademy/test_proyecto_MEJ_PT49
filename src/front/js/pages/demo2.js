dbcb650223b6b594711597dad8f5f915ec54471a
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2024-01-06 21:22:51 +0000

    volviendo a Main y cambiando pequeñas cosas del Demo 2

--- src/front/js/pages/demo2.js
+++ src/front/js/pages/demo2.js
@@ -5,6 +5,7 @@ import { Context } from "../store/appContext";
 export const Demo2 = () => {
     const { store, actions } = useContext(Context);
     const [selectedOption, setSelectedOption] = useState(null);
+
     useEffect(() => {
         actions.getMovieListFromApi2();
     }, []);
@@ -17,36 +18,41 @@ export const Demo2 = () => {
         setSelectedOption(event.target.value);
     };
 
-
     return (
         <>
-                <select onChange={handleSelectChange}>
-                    <option disabled selected>Selecciona una opción</option>
-                    <option value={"funny"}>Funny</option>
-                    <option value={"sad"}>Sad</option>
-                </select>
-                {selectedOption === "funny" && (
-                    <button onClick={() => actions.getFunnyRecommendation()}>Una peli funny!</button>
-                )}
-                {selectedOption === "sad" && (
-                    <button onClick={() => actions.getSadRecommendation()}>Una peli sad!</button>
-                )}
-                {store.recommendedFunnyMovie && (
-                    <div>
-                        <h2>{store.recommendedFunnyMovie.name}</h2>
-                        <p>{store.recommendedFunnyMovie.description}</p>
-                        <img src={store.recommendedFunnyMovie.poster} alt={store.recommendedFunnyMovie.name} />
-                        <p>Fecha de lanzamiento: {store.recommendedFunnyMovie.relese_date}</p>
-                    </div>
-                )}
-                {store.recommendedSadMovie && (
-                    <div>
-                        <h2>{store.recommendedSadMovie.name}</h2>
-                        <p>{store.recommendedSadMovie.description}</p>
-                        <img src={store.recommendedSadMovie.poster} alt={store.recommendedSadMovie.name} />
-                        <p>Fecha de lanzamiento: {store.recommendedSadMovie.relese_date}</p>
-                    </div>
-                )}
+            <select value={selectedOption} onChange={handleSelectChange}>
+                <option disabled>Select an option</option>
+                <option value={"funny"}>Funny</option>
+                <option value={"sad"}>Sad</option>
+            </select>
+
+            {selectedOption === "funny" && (
+                <div>
+                    <button onClick={() => actions.getFunnyRecommendation()}>Get a funny movie!</button>
+                    {store.recommendedFunnyMovie && (
+                        <div>
+                            <h2>{store.recommendedFunnyMovie.name}</h2>
+                            <p>{store.recommendedFunnyMovie.description}</p>
+                            <img src={store.recommendedFunnyMovie.poster} alt={store.recommendedFunnyMovie.name} />
+                            <p>Release date: {store.recommendedFunnyMovie.relese_date}</p>
+                        </div>
+                    )}
+                </div>
+            )}
+
+            {selectedOption === "sad" && (
+                <div>
+                    <button onClick={() => actions.getSadRecommendation()}>Get a sad movie!</button>
+                    {store.recommendedSadMovie && (
+                        <div>
+                            <h2>{store.recommendedSadMovie.name}</h2>
+                            <p>{store.recommendedSadMovie.description}</p>
+                            <img src={store.recommendedSadMovie.poster} alt={store.recommendedSadMovie.name} />
+                            <p>Release date: {store.recommendedSadMovie.relese_date}</p>
+                        </div>
+                    )}
+                </div>
+            )}
         </>
     );
 };
